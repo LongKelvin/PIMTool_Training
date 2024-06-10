8c37261f885be8c79be1724ce62f2e28c49aa308
KLO1HC <long.k@vn.bosch.com>
2024-06-10 17:05:48 +0700

    [Feature] implemented new custom MultiSelectBox control

---
+++ PIMTool.Winforms/UserControls/CustomControls/MultiSelectPillbox.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace PIMTool.Winforms.UserControls.CustomControls
+{
+    partial class MultiSelectPillbox
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.ListBox listBox;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.searchTextBox.Location = new System.Drawing.Point(0, 0);
+            this.searchTextBox.Name = "searchTextBox";
+           this.searchTextBox.Size = new System.Drawing.Size(150, 20);
+         
+            this.searchTextBox.TabIndex = 0;
+            this.searchTextBox.TextChanged += new System.EventHandler(this.SearchTextBox_TextChanged);
+            this.searchTextBox.GotFocus += new System.EventHandler(this.SearchTextBox_GotFocus);
+            this.searchTextBox.LostFocus += new System.EventHandler(this.SearchTextBox_LostFocus);
+
+            // 
+            // listBox
+            // 
+            this.listBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(0, 20);
+            this.listBox.Name = "listBox";
+            this.listBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox.Size = new System.Drawing.Size(150, 130);
+            this.listBox.TabIndex = 1;
+            this.listBox.Visible = false;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.ListBox_SelectedIndexChanged);
+            this.listBox.LostFocus += new System.EventHandler(this.ListBox_LostFocus);
+            this.listBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListBox_MouseDown);
+            this.listBox.MouseClick += ListBox_MouseClick;
+
+            this.listBox.SelectionMode = SelectionMode.MultiSimple;
+
+
+            // 
+            // MultiSelectPillbox
+            // 
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.searchTextBox);
+            this.Name = "MultiSelectPillbox";
+            this.Size = new System.Drawing.Size(150, 150);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ PIMTool.Winforms/UserControls/CustomControls/MultiSelectPillbox.cs
@@ -0,0 +1,156 @@
+﻿using System.ComponentModel;
+
+namespace PIMTool.Winforms.UserControls.CustomControls
+{
+    public partial class MultiSelectPillbox : UserControl
+    {
+        private List<string>? items;
+        private readonly HashSet<string> selectedItems = [];
+
+        private bool listBoxSelectionChanging;
+
+        public MultiSelectPillbox()
+        {
+            InitializeComponent();
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public List<string>? DataSource
+        {
+            get => items;
+            set
+            {
+                items = value;
+                UpdateItems();
+            }
+        }
+
+        private void UpdateItems()
+        {
+            listBox.Items.Clear();
+            if (items != null)
+            {
+                listBox.Items.AddRange([.. items]);
+            }
+        }
+
+        private void SearchTextBox_GotFocus(object sender, EventArgs e)
+        {
+            ShowList();
+        }
+
+        private void SearchTextBox_LostFocus(object sender, EventArgs e)
+        {
+            BeginInvoke(new Action(() =>
+            {
+                if (!listBox.Focused)
+                {
+                    listBox.Visible = false;
+                }
+            }));
+        }
+
+        private void ListBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && listBox.SelectedIndex != ListBox.NoMatches)
+            {
+                UpdateSelectedItems();
+                UpdateSearchBox();
+            }
+        }
+
+        private void ListBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            int index = listBox.IndexFromPoint(e.Location);
+            if (index != ListBox.NoMatches)
+            {
+                listBoxSelectionChanging = true;
+                listBox.SelectedIndex = index;
+                listBoxSelectionChanging = false;
+            }
+        }
+
+        private void ListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!listBoxSelectionChanging)
+            {
+                UpdateSelectedItems();
+                UpdateSearchBox();
+            }
+        }
+
+        private void UpdateSearchBox()
+        {
+            searchTextBox.Text = string.Join(";", selectedItems) + ";";
+        }
+
+        private void SearchTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string filter = searchTextBox.Text.Contains(';')
+                ? searchTextBox.Text.Split(';').LastOrDefault()!.ToLower()
+                : searchTextBox.Text.ToLower();
+
+            var filteredItems = items?.Where(item => item.Contains(filter, StringComparison.CurrentCultureIgnoreCase) && !selectedItems.Contains(item)).ToList() ?? [];
+            listBox.Items.Clear();
+            listBox.Items.AddRange([.. filteredItems]);
+
+            string[] remainItems = searchTextBox.Text.Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+            selectedItems.Clear();
+            foreach (string item in remainItems)
+            {
+                if (items?.Contains(item) == true && !selectedItems.Contains(item))
+                {
+                    selectedItems.Add(item);
+                }
+            }
+
+            ShowList();
+        }
+
+        private void ListBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            int index = listBox.IndexFromPoint(e.Location);
+            if (index != ListBox.NoMatches)
+            {
+                listBox.SelectedIndex = index;
+                UpdateSelectedItems();
+                UpdateSearchBox();
+            }
+        }
+
+        private void UpdateSelectedItems()
+        {
+            foreach (string item in listBox.SelectedItems)
+            {
+                selectedItems.Add(item);
+            }
+
+            this.Text = string.Join(";", selectedItems) + ";";
+            listBox.Refresh();
+        }
+
+        private void ShowList()
+        {
+            listBox.Visible = listBox.Items.Count > 0;
+            listBox.BringToFront();
+        }
+
+        public List<string> GetSelectedItems()
+        {
+            return [.. selectedItems];
+        }
+
+        private void ListBox_LostFocus(object sender, EventArgs e)
+        {
+            BeginInvoke(new Action(() =>
+            {
+                if (!searchTextBox.Focused)
+                {
+                    listBox.Visible = false;
+                }
+            }));
+        }
+
+    }
+}
